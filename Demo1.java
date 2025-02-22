e6cdba77e40ce3911e4217c3b403345e55d685d0
Farman396 <farmangada59@gmail.com>
2025-02-22 15:42:07 -0800

    change in Demo1 file

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,10 @@
 class Demo{
     public static void main(String[] args){
         System.out.println("Hello");
+    
+    }
+
+    public void m1(){
+        System.out.println("method m1");
     }
 }
