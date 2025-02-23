ecc9753fec0e1e69ba4bc2d5c2ad9265b4296b23
Farman396 <farmangada59@gmail.com>
2025-02-22 16:50:32 -0800

    test

--- Demo1.java
+++ Demo1.java
@@ -1,11 +1,13 @@
 class Demo{
     public static void main(String[] args){
         System.out.println("Hiiiiiiiiiiiiiiiii");
+        System.out.println("byy byy");
     
     }
 
     public void payment(){
         System.out.println("payment SUccesfully...");
+        System.out.println("Done");
     }
 
     public void notifyUser(){
